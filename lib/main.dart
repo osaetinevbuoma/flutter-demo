0aaf0a3bc4db705e9d0a12088b408935ba2b2936
Osaetin Evbuoma <osaetinevbuoma@gmail.com>
2021-05-30 08:57:39 +0100

    State management

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_layout/favorite.dart';
 
 void main() => runApp(MyApp());
 
@@ -104,11 +105,7 @@ class MyApp extends StatelessWidget {
                 ],
               ),
             ),
-            Icon(
-              Icons.star,
-              color: Colors.red[500],
-            ),
-            Text('41'),
+            FavoriteWidget(),
           ],
         ),
       );
